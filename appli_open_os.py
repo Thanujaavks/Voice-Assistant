f7be0e4f6a6ec2d225faa430da7219885a7690c0
Venkadakrishnasharma <101462622+Thanujaavks@users.noreply.github.com>
2024-06-18 16:24:27 +0530

    changes

---
+++ appli_open_os.py
@@ -0,0 +1,4 @@
+import os
+
+path = "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Accessories"
+os.startfile(path)
---
+++ appli_open_sub.py
@@ -0,0 +1,4 @@
+import subprocess
+
+path =r"C:\\Users\\Hello\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe"
+subprocess.call(path)
